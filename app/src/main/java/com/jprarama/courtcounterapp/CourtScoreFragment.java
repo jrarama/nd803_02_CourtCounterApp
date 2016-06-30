46caa7f9c95c8a3602b10b56006d4e09769ed525
jrarama <joshuararama@gmail.com>
2016-07-01 01:57:14 +0800

    feat: Added Button Functionality

--- app/src/main/java/com/jprarama/courtcounterapp/CourtScoreFragment.java
+++ app/src/main/java/com/jprarama/courtcounterapp/CourtScoreFragment.java
@@ -6,16 +6,63 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by joshua on 1/7/16.
  */
 public class CourtScoreFragment extends Fragment {
 
+    private TextView tvTeamName;
+    private TextView tvScore;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.court_score_item, container, false);
+        tvTeamName = (TextView) rootView.findViewById(R.id.teamName);
+        tvScore = (TextView) rootView.findViewById(R.id.score);
+
+        Button btn1Point = (Button) rootView.findViewById(R.id.btn1point);
+        Button btn2Points = (Button) rootView.findViewById(R.id.btn2points);
+        Button btn3Points = (Button) rootView.findViewById(R.id.btn3points);
+
+        btn1Point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increaseScore(1);
+            }
+        });
+
+        btn2Points.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increaseScore(2);
+            }
+        });
+
+        btn3Points.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increaseScore(3);
+            }
+        });
+
         return rootView;
+    }
+
+    private void increaseScore(int points) {
+        int score = Integer.parseInt(tvScore.getText().toString().trim()) + points;
+        String newScore = String.format("%02d", score);
+        tvScore.setText(newScore);
+    }
+
+    public TextView getTvTeamName() {
+        return tvTeamName;
+    }
+
+    public void resetScore() {
+        tvScore.setText("00");
     }
 }
--- app/src/main/java/com/jprarama/courtcounterapp/MainActivity.java
+++ app/src/main/java/com/jprarama/courtcounterapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jprarama.courtcounterapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        final CourtScoreFragment teamA = (CourtScoreFragment) fm.findFragmentById(R.id.fragmentTeamA);
+        final CourtScoreFragment teamB = (CourtScoreFragment) fm.findFragmentById(R.id.fragmentTeamB);
+
+        teamA.getTvTeamName().setText("Team A");
+        teamB.getTvTeamName().setText("Team B");
+
+        Button reset = (Button) findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                teamA.resetScore();
+                teamB.resetScore();
+            }
+        });
     }
+
 }
